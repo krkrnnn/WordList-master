dcf36e8c55ff945465adfad47ac319030c9133c4
福田かるな <ring.ring.0114@gmail.com>
2016-02-12 21:56:18 +0900

    単語帳　完成

---
+++ WordList/AddViewController.swift
@@ -0,0 +1,87 @@
+//
+//  AddViewController.swift
+//  WordList
+//
+//  Created by 福田かるな on 2016/02/09.
+//  Copyright © 2016年 Karuna. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    
+    @IBOutlet var englishTextField: UITextField!
+    @IBOutlet var japaneseTextField: UITextField!
+    
+    var wordArray: [AnyObject] = []
+    let saveData = NSUserDefaults.standardUserDefaults()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if saveData.arrayForKey("WORD") != nil{
+            wordArray = saveData.arrayForKey("WORD")!
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveWord(){
+        let wordDictionary = ["english": englishTextField.text!,"japanese": japaneseTextField.text!]         
+        if englishTextField.text == "" || japaneseTextField.text == "" {
+            let alert = UIAlertController(
+                title: "保存失敗",
+                message: "テキスト入力欄に入力してください",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(
+                UIAlertAction(
+                    title: "OK",
+                    style: UIAlertActionStyle.Default,
+                    handler: nil
+                )
+            )
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            wordArray.append(wordDictionary)
+            saveData.setObject(wordArray, forKey: "WORD")
+            
+            let alert = UIAlertController(
+                title: "保存完了",
+                message: "単語の登録が完了しました",
+                preferredStyle:  UIAlertControllerStyle.Alert)
+            alert.addAction(
+                UIAlertAction(
+                    title: "OK",
+                    style: UIAlertActionStyle.Default,
+                    handler: nil
+                )
+            )
+            self.presentViewController(alert, animated: true, completion: nil)
+            englishTextField.text = ""
+            japaneseTextField.text = ""
+        }
+        
+        
+    }
+
+  
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WordList/ListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ListTableViewCell.swift
+//  WordList
+//
+//  Created by 福田かるな on 2016/02/09.
+//  Copyright © 2016年 Karuna. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet var englishLabel: UILabel!
+    @IBOutlet var japaneseLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
